b7e329c33d26aee7140838bcca6af8e07e2445e2
Avvud <avvudaiyappanrm@gmail.com>
2025-10-21 18:05:33 +0530

    Update homesections.jsx

--- src/pages/homesections.jsx
+++ src/pages/homesections.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { FaPalette, FaShippingFast, FaCertificate, FaHeart, FaUsers } from "react-icons/fa";
-import '../styles/HomeSection.css';
+import '../styles/homeSection.css';
 
 // ✅ STATS SECTION
 export function StatsSection({ stats, isVisible }) {
