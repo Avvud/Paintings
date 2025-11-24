bcd413a1edfed631341aed2ecc24a6d7ac027e15
Avvud <avvudaiyappanrm@gmail.com>
2025-11-24 23:15:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Murugar from './pages/murugar';
 function App() {
   return (
     <>
-      <script>window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}</script><script id="zsiqscript" src="https://salesiq.zohopublic.com/widget?wc=siqba5104846f5756656ffd93530dcfcd95a90dc4652ea2d64495c6fda435171406" defer></script>
       <NavBar />   {/*  <—– always visible  */}
       
       <Routes>
